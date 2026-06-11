e4987fe278796082fede970e97685c5c27e96e2d
itsomrai <work.omrai@gmail.com>
2026-06-11 21:27:56 +0530

    basics for beginners

--- Arrays & Strings/Basics for beginners/simpleSearchLinear.c
+++ Arrays & Strings/Basics for beginners/simpleSearchLinear.c
@@ -1,24 +1,25 @@
+//Simple code doing linear search
 #include <stdio.h>
 int main()
 {
 int arr[]={25,10,30,40,50,60};
-int target=20;
+int target=20;// input already mentioned
 int size=sizeof(arr)/sizeof(arr[0]);
 int foundIndex=-1;
 
 //Loop through the array
 for(int i=0;i<size;i++){
     if(arr[i]==target){
-        foundIndex=i;
+        foundIndex=i;//This is used for finding index where the elment is located
         break;
     }
 }
 if(foundIndex != -1){
-    printf("Element found at index %d\n", foundIndex);
+    printf("Element found at index %d\n", foundIndex);//element found
 
 }
 else{
-    printf("Error! Not found\n");
+    printf("Error! Not found\n");//Element Not found
 }
 return 0;
 }
